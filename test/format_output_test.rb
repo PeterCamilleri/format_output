898ead23d3321e5f50db8183d58523d39cd9358e
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-12-16 12:47:23 -0500

    Initial commit.

--- test/format_output_test.rb
+++ test/format_output_test.rb
@@ -1,11 +1,17 @@
-require "test_helper"
+require_relative '../lib/format_output'
+gem              'minitest'
+require          'minitest/autorun'
+require          'minitest_visible'
 
 class FormatOutputTest < Minitest::Test
+
+  #Track mini-test progress.
+  include MinitestVisible
+
   def test_that_it_has_a_version_number
-    refute_nil ::FormatOutput::VERSION
+    refute_nil(::FormatOutput::VERSION)
+    assert(::FormatOutput::VERSION.is_a?(String))
+    assert(/\A\d+\.\d+\.\d+/ =~ ::FormatOutput::VERSION)
   end
 
-  def test_it_does_something_useful
-    assert false
-  end
 end
--- test/test_helper.rb
+++
@@ -1,4 +0,0 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "format_output"
-
-require "minitest/autorun"
