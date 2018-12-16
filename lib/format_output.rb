5a9afb2cc036b0daf1b02d54987b79319633ae2f
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-12-16 14:09:17 -0500

    Connected to the new builder files.

--- lib/format_output.rb
+++ lib/format_output.rb
@@ -1,7 +1,9 @@
 # coding: utf-8
 
-require_relative "format_output/builders/bullets"
-require_relative "format_output/builders/columns"
+require_relative "format_output/columns/columns"
+
+require_relative "format_output/builders/bullet_builder"
+require_relative "format_output/builders/column_builder"
 require_relative "format_output/version"
 
 # The format output facility. Neat columns and bullet points for all!
