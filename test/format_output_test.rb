ce4e936f7e384a5edc1db5171d94c72a2544790c
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-12-17 11:22:37 -0500

    Fixed namespace path in tests.

--- test/format_output_test.rb
+++ test/format_output_test.rb
@@ -27,14 +27,14 @@ class FormatOutputTest < Minitest::Test
   end
 
   def test_that_keeps_a_page_width
-    assert_equal(80, FormatOutput.page_width)
-    FormatOutput.page_width = '40'
-    assert_equal(40, FormatOutput.page_width)
-    assert_raises {FormatOutput.page_width = 2}
-    assert_raises {FormatOutput.page_width = 'apple'}
-    assert_equal(40, FormatOutput.page_width)
-    FormatOutput.page_width = 80
-    assert_equal(80, FormatOutput.page_width)
+    assert_equal(80, ::FormatOutput.page_width)
+    ::FormatOutput.page_width = '40'
+    assert_equal(40, ::FormatOutput.page_width)
+    assert_raises {::FormatOutput.page_width = 2}
+    assert_raises {::FormatOutput.page_width = 'apple'}
+    assert_equal(40, ::FormatOutput.page_width)
+    ::FormatOutput.page_width = 80
+    assert_equal(80, ::FormatOutput.page_width)
   end
 
 end
