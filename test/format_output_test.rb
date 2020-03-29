03909f4265b4a5b9526eaaaee4197881dfbfa1fa
PeterCamilleri <peter.c.camilleri@gmail.com>
2020-03-29 09:51:26 -0400

    Removed dependency on minitest_visible for issue #4

--- test/format_output_test.rb
+++ test/format_output_test.rb
@@ -1,12 +1,8 @@
 require_relative '../lib/format_output'
 gem              'minitest'
 require          'minitest/autorun'
-require          'minitest_visible'
 
 class FormatOutputTest < Minitest::Test
-
-  #Track mini-test progress.
-  include MinitestVisible
 
   def test_that_it_has_a_version_number
     refute_nil(::FormatOutput::VERSION)
